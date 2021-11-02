3f76a817d464a919957635a46ba0d59015dc5982
Mario Solis <56165442+msolis6686@users.noreply.github.com>
2021-11-02 19:59:15 -0300

    util

---
+++ utilitarios/general_ledger_xlsx/__manifest__.py
@@ -0,0 +1,20 @@
+# Author: Damien Crier
+# Author: Julien Coux
+# Copyright 2016 Camptocamp SA
+# Copyright 2020 ForgeFlow S.L. (https://www.forgeflow.com)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Ledger xlsx",
+    "version": "0.0.1",
+    "category": "",
+    "summary": "Blackfhis",
+    "author": 
+    "Blackfhis",
+    "website": "",
+    "depends": ["account", "account_financial_report", "report_xlsx"],
+    "data": [
+    ],
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+}
---
+++ utilitarios/sale_report_py3o/__manifest__.py
@@ -0,0 +1,31 @@
+# Copyright 2016-2021 Akretion France (https://akretion.com/)
+# @author: Alexis de Lattre <alexis.delattre@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Sale Reports Py3o',
+    'version': '14.0.1.0.0',
+    'category': 'Sales',
+    'license': 'AGPL-3',
+    'summary': 'Sample py3o sale reports',
+    'description': """
+Sale Reports Py3o
+=================
+
+This module adds a sample py3o sale reports.
+
+This module has been written by Alexis de Lattre from Akretion
+<alexis.delattre@akretion.com>.
+    """,
+    'author': 'Akretion',
+    'depends': [
+        'report_py3o',
+        'sale_commercial_partner',
+        'base_company_extension',
+        'base_usability',  # to have res_partner.name_title
+        'sale_usability',  # for layout
+        'account_payment_sale',
+        ],
+    'data': ['report.xml'],
+    'installable': True,
+}
