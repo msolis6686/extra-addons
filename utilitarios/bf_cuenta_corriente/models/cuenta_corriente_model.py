a93cf19c352f25194b6f0744be8c0cbd25c23fd9
Mario Solis <56165442+msolis6686@users.noreply.github.com>
2021-12-08 22:43:48 -0300

    add

---
+++ utilitarios/bf_cuenta_corriente/models/cuenta_corriente_model.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, _
+
+class bf_cuenta_corriente(models.Model):
+    #_name = 'bf.cuenta.corriente'
+    _inherit = 'res.partner'
+    _description = 'Modulo para mostrar las cuentas corrientes de los clientes'
+
+    debe = fields.Float(compute='_get_debe', string="Debe")
+    haber = fields.Float(compute='_get_haber', string="Haber")
+    saldo = fields.Float(compute='_get_saldo', string="Saldo")
+    
+
+    def _get_debe(self):
+        for r in self:
+            facturas = self.env['account.move'].search([('partner_id', '=', r.id),('state', '=', 'posted'),'|',('type', '=', 'out_invoice'),('type','=','out_refund')])
+            total_debe = 0
+            for x in facturas:
+                if x.type == 'out_refund':
+                    total_debe = total_debe - x.amount_total_signed
+                else:
+                    total_debe = total_debe + x.amount_total_signed
+            r.debe = total_debe
+
+    def _get_haber(self):
+        for r in self:
+            payment = self.env['account.payment'].search([('partner_id', '=', r.id)])
+            total_haber = 0
+            for x in payment:
+                total_haber = total_haber + x.amount
+            r.haber = total_haber
+
+    def _get_saldo(self):
+        for r in self:
+            total_debe = r.debe - r.haber
+            r.saldo = total_debe
+
+    def action_show_invoices(self):
+        invoices = self.env["account.move"].search([('partner_id', '=', self.id),('state', '=', 'posted'),'|',('type', '=', 'out_invoice'),('type','=','out_refund')])
+        #for contract in self.contract_to_invoice_ids:
+            #invoices |= contract.recurring_create_invoice()
+        return {
+            "type": "ir.actions.act_window",
+            "name": _("Invoices"),
+            "res_model": "account.move",
+            "domain": [("id", "in", invoices.ids)],
+            "view_mode": "tree,form",
+            "context": self.env.context,
+        }
+              
+    def action_show_payments(self):
+        payments = self.env["account.payment"].search([('partner_id', '=', self.id),('state', '=', 'posted')])
+        #for contract in self.contract_to_invoice_ids:
+            #invoices |= contract.recurring_create_invoice()
+        return {
+            "type": "ir.actions.act_window",
+            "name": _("Invoices"),
+            "res_model": "account.payment",
+            "domain": [("id", "in", payments.ids)],
+            "view_mode": "tree,form",
+            "context": self.env.context,
+        }
--- utilitarios/web_widget_url_advanced/readme/CONTRIBUTORS.rst
+++ utilitarios/web_widget_url_advanced/readme/CONTRIBUTORS.rst
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 * Simone Orsi <simone.orsi@camptocamp.com>
 * `CorporateHub <https://corporatehub.eu/>`__
 
@@ -6,3 +7,13 @@
 * Jay Vora <jay.vora@serpentcs.com>
 * Swapnesh Shah <s.shah.serpentcs@gmail.com>
 * Joan Sisquella <joan.sisquella@forgeflow.com>
+=======
+* Simone Orsi <simone.orsi@camptocamp.com>
+* `CorporateHub <https://corporatehub.eu/>`__
+
+  * Alexey Pelykh <alexey.pelykh@corphub.eu>
+
+* Jay Vora <jay.vora@serpentcs.com>
+* Swapnesh Shah <s.shah.serpentcs@gmail.com>
+* Joan Sisquella <joan.sisquella@forgeflow.com>
+>>>>>>> fb07532a54a03835d219cf560d8d6420e65c08a9
