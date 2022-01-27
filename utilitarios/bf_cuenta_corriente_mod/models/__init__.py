c980325541e333d128340b6976d2b70b0f61db5d
Mario Solis <56165442+msolis6686@users.noreply.github.com>
2022-01-27 07:31:45 -0300

    cuenta corriente mod

---
+++ utilitarios/bf_cuenta_corriente_mod/models/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+
+from . import cuenta_corriente_mod
---
+++ utilitarios/bf_cuenta_corriente_mod/models/cuenta_corriente_mod.py
@@ -0,0 +1,188 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, _
+from icecream import ic
+
+class bf_cuenta_corriente_mod(models.Model):
+    #_name = 'bf.cuenta.corriente'
+    _inherit = 'res.partner'
+    _description = 'Modulo para mostrar las cuentas corrientes de los clientes'
+
+    debe_m = fields.Float(string="Debe", related="debe_c", default = 0.0, store=True)
+    haber_m = fields.Float(string="Haber", related="haber_c", default = 0.0,  store=True)
+    saldo_m = fields.Float(string="Saldo", related="saldo_c", default = 0.0, store=True)
+
+    debe_c = fields.Float(compute='_get_debe_computed', default = 0.0, string="Debe Calculado")
+    haber_c = fields.Float(compute='_get_haber_computed', default = 0.0, string="Haber Calculado")
+    saldo_c = fields.Float(compute='_get_saldo_computed', default = 0.0, string="Saldo Calculado")
+    
+    def _get_debe_c(self):
+        for r in self:
+            facturas = self.env['account.move'].search([('partner_id', '=', r.id),('state', '=', 'posted'),'|',('type', '=', 'out_invoice'),('type','=','out_refund')])
+            total_debe = 0
+            ##### AL ENCONTRAR UNA NOTA DE CREDITO, TOMA EL VALOR COMO NEGATIVO, SI LO QUERES RESTAR, LO SUMA (-*- = +)
+            for x in facturas:
+                total_debe = total_debe + x.amount_total_signed
+            r.debe_c = total_debe
+            if r.debe_c > r.debe_m:
+                diff = r.debe_c - r.debe_m
+            else:
+                diff = r.debe_m - r.debe_c
+            
+            if diff > 0.1:
+                ic("ACTUALIZO DEBE")
+                ic(r.id)
+                vals = {'debe_m': r.debe_c}
+                r.write(vals)
+                self.reload_page()
+    
+    def _get_haber_c(self):
+        for r in self:
+            payment = self.env['account.payment'].search([('partner_id', '=', r.id),('state', '=', 'posted')])
+            total_haber = 0
+            for x in payment:
+                total_haber = total_haber + x.amount
+            r.haber_c = total_haber
+            
+            if r.haber_c > r.haber_m:
+                diff = r.haber_c - r.haber_m
+            else:
+                diff = r.haber_m - r.haber_c
+            
+            if diff > 0.1:
+                ic("ACTUALIZO HABER")
+                ic(r.id)
+                ic(r.haber_c)
+                vals = {'haber_m': r.haber_c}
+                r.write(vals)
+                self.reload_page()
+
+    def _get_saldo_c(self):
+        for r in self:
+            total_saldo = r.debe_c - r.haber_c
+            r.saldo_c = total_saldo
+            if r.saldo_c > r.saldo_m:
+                diff = r.saldo_c - r.saldo_m
+            else:
+                diff = r.saldo_m - r.saldo_c
+            
+            if diff > 0.1:
+                ic("ACTUALIZO SALDOS")
+                ic(r.id)
+                ic(r.saldo_c)
+                ic(r.saldo_m)
+                vals = {'saldo_m': r.saldo_c}
+                r.write(vals)
+                self.reload_page()
+    
+    def _get_debe_computed(self):
+        for r in self:
+            facturas = self.env['account.move'].search([('partner_id', '=', r.id),('state', '=', 'posted'),'|',('type', '=', 'out_invoice'),('type','=','out_refund')])
+            total_debe = 0
+            ##### AL ENCONTRAR UNA NOTA DE CREDITO, TOMA EL VALOR COMO NEGATIVO, SI LO QUERES RESTAR, LO SUMA (-*- = +)
+            for x in facturas:
+                total_debe = total_debe + x.amount_total_signed
+            r.debe_c = total_debe
+            diff=0
+            if r.debe_c > r.debe_m:
+                diff = r.debe_c - r.debe_m
+            else:
+                diff = r.debe_m - r.debe_c
+            
+            if diff > 0.01:
+                ic("ACTUALIZO DEBE")
+                ic(r.id)
+                vals = {'debe_m': r.debe_c}
+                r.write(vals)
+                #self.reload_page()
+    
+    def _get_haber_computed(self):
+        for r in self:
+            payment = self.env['account.payment'].search([('partner_id', '=', r.id),('state', '=', 'posted')])
+            total_haber = 0
+            for x in payment:
+                total_haber = total_haber + x.amount
+            r.haber_c = total_haber
+            diff=0
+            if r.haber_c > r.haber_m:
+                diff = r.haber_c - r.haber_m
+            else:
+                diff = r.haber_m - r.haber_c
+            
+            if diff > 0.01:
+                ic("ACTUALIZO HABER")
+                ic(r.id)
+                ic(r.haber_c)
+                vals = {'haber_m': r.haber_c}
+                r.write(vals)
+                #self.reload_page()
+    
+    def _get_saldo_computed(self):
+        for r in self:
+            total_saldo = r.debe_m - r.haber_m
+            r.saldo_c = total_saldo
+            diff = 0
+            if r.saldo_c > r.saldo_m:
+                diff = r.saldo_c - r.saldo_m
+            else:
+                diff = r.saldo_m - r.saldo_c
+            
+            if diff >= 0.01:
+                ic("ACTUALIZO SALDOS")
+                ic(r.id)
+                ic(r.saldo_c)
+                ic(r.saldo_m)
+                vals = {'saldo_m': r.saldo_c}
+                r.write(vals)
+    
+    def action_show_invoices(self):
+        invoices = self.env["account.move"].search([('partner_id', '=', self.id),('state', '=', 'posted'),'|',('type', '=', 'out_invoice'),('type','=','out_refund')])
+        #for contract in self.contract_to_invoice_ids:
+            #invoices |= contract.recurring_create_invoice()
+        return {
+            "type": "ir.actions.act_window",
+            "name": _("Invoices"),
+            "res_model": "account.move",
+            "domain": [("id", "in", invoices.ids)],
+            "view_mode": "tree,form",
+            "context": self.env.context,
+        }
+              
+    def action_show_payments(self):
+        payments = self.env["account.payment"].search([('partner_id', '=', self.id),('state', '=', 'posted')])
+        #for contract in self.contract_to_invoice_ids:
+            #invoices |= contract.recurring_create_invoice()
+        return {
+            "type": "ir.actions.act_window",
+            "name": _("Invoices"),
+            "res_model": "account.payment",
+            "domain": [("id", "in", payments.ids)],
+            "view_mode": "tree,form",
+            "context": self.env.context,
+        }
+    
+    #Funcion para que el boton llame directamente al wizard del partner statement
+    def button_export_pdf(self):
+        view_id = self.env.ref('partner_statement.activity_statement_wizard_view').id#Este dato lo sacamos de Ajustes/Tecnico/Vistas. Es el ID externo
+        return {'type': 'ir.actions.act_window',#El type tiene que ser el mismo que usa el wizard (act_window)
+                'name': _('Estado de la cuenta del cliente'),#Nombre que va a tener el wizard.
+                'res_model': 'activity.statement.wizard',#El modelo del wizard o de la vista (se lo puede sacar del codigo, es el campo _name="nombre")
+                'target': 'new',#New para que se abra una nueva ventana (el wizard)
+                'view_mode': 'form',#Modo de vista (formulario para el wizard)
+                'views': [[view_id, 'form']],#El id externo de la vista que definimos en la variable mas arriba y el 'form' o tree segun necesitemos
+                }
+    
+    def reload_page(self):
+        model_obj = self.env['ir.model.data']
+        data_id = model_obj._get_id('bf_cuenta_corriente_mod', 'bf_cuenta_corriente_mod_view')
+        view_id = model_obj.browse(data_id).res_id
+        return {
+            'type': 'ir.actions.act_window',
+            'name': _('Cuenta corriente de clientes'),
+            'res_model': 'bf_cuenta_corriente_mod',
+            'view_type' : 'tree',
+            'view_mode' : 'form',
+            'view_id' : view_id,
+            'target' : 'current',
+            'nodestroy' : False,
+        }
