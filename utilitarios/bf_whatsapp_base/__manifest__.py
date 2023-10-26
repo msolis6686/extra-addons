82e842decbf1d5fbca738fdec471fabb11d5cf61
Mario Solis <56165442+msolis6686@users.noreply.github.com>
2023-10-26 16:40:00 -0300

    UPD wa

--- utilitarios/bf_whatsapp_base/__manifest__.py
+++ utilitarios/bf_whatsapp_base/__manifest__.py
@@ -3,15 +3,15 @@
     'name': "bf_whatsapp_base",
 
     'summary': """
-        Este módulo se conecta con un servicio de whatsapp y permite enviar mensajes a sus clientes.
+        Este módulo se conecta con un servicio de API de whatsapp y permite enviar mensajes a sus clientes.
         """,
 
     'description': """
-        Este módulo se conecta con un servicio de whatsapp y permite enviar mensajes a sus clientes.
+        Este módulo se conecta con un servicio de API de whatsapp y permite enviar mensajes a sus clientes.
     """,
 
     'author': "BlackFishTeam",
-    'website': "https://www.blackfishweb.com/",
+    'website': "https://www.blackfish.com.ar/",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
@@ -28,19 +28,19 @@
         'security/ir.model.access.csv',
         'views/views.xml',
         #'views/templates.xml',
-        'views/template.xml',
-        'views/account_invoice.xml',
+        #'views/template.xml',
+        #'views/account_invoice.xml',
         'views/res_partner.xml',
         'wizard/create_message.xml',
         'wizard/massive_wa_send_wizard.xml',
         'wizard/message_wizard.xml',
         'wizard/wizard_contact.xml',
         'wizard/wizard.xml',
-        'data/demo.xml',
+        'data/template.xml',
         'data/registros.xml',
     ],
     # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
+    #'demo': [
+    #    'demo/demo.xml',
+    #],
 }
--- utilitarios/bf_whatsapp_base/models/__init__.py
+++ utilitarios/bf_whatsapp_base/models/__init__.py
@@ -2,7 +2,7 @@
 
 from . import whatsapp_config
 from . import whatsapp_message
-from . import whatsapp_templates
+#from . import whatsapp_templates
 from . import account_move
 from . import res_partner
 from . import contacts_fun
