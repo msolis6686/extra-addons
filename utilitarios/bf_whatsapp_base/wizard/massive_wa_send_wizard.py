d46fca1fce3b5c2b8a6c8b28f45f171ce192a918
Foca <gonzalojovanovich@outlook.com>
2023-11-03 16:55:50 -0300

    Removido un <<<< HEAD

--- utilitarios/bf_whatsapp_base/wizard/massive_wa_send_wizard.py
+++ utilitarios/bf_whatsapp_base/wizard/massive_wa_send_wizard.py
@@ -4,19 +4,8 @@ from odoo.exceptions import UserError, ValidationError
 class BfMassiveWaSendWizard(models.TransientModel):
     _name = "bf.massive.wa.send.wizard"
     _description = "Massive Whats App Messages Wizard"
-<<<<<<< HEAD
-    
-    
-    def get_default_wa_template(self):
-        template = self.get_whatsapp_template()
-        if template:
-            return template
-    
-    wa_template = fields.Many2one(comodel_name="bf.whatsapp.templates", default=get_default_wa_template)
-=======
 
     wa_template = fields.Many2one(comodel_name="mail.template")
->>>>>>> origin/dev_wa
     error = fields.Boolean(default=True)
 
     @api.onchange("wa_template")
@@ -33,23 +22,6 @@ class BfMassiveWaSendWizard(models.TransientModel):
         if len(no_wa_clients) > 1:
             # Remove the ", " in the error message.
             no_wa_clients = no_wa_clients[:-2]
-<<<<<<< HEAD
-            raise UserError(_(f"Los siguientes contactos no tienen un número de WhatsApp asignado: {no_wa_clients}"))
-        self.error = False
-
-    def massive_prepare(self):
-        to_create = self.env['bf.whatsapp.create.messages'].create({
-            'template_id': self.wa_template.id
-        })
-        to_create.prepare_messages()
-    
-    def get_whatsapp_template(self):
-        context_model = self._context.get('active_model')
-        if context_model:
-            wa_template = self.env['bf.whatsapp.templates'].search([('model', '=', context_model)], limit=1)
-            if wa_template:
-                return wa_template.id
-=======
             raise UserError(
                     _("Los siguientes clientes no tienen telefono de WhatsApp" 
                         f"asignado: {no_wa_clients}")
@@ -94,4 +66,3 @@ class BfMassiveWaSendWizard(models.TransientModel):
             )
             temp_record.with_context(active_id=id).onchange_template_id_wrapper()
             temp_record.prepare_messages()
->>>>>>> origin/dev_wa
